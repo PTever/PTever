10827c04ce62edbffe4fa4b2fe1e18e83852408f
infiniment <smnoh0707@gmail.com>
2023-08-25 16:18:08 +0900

    loginPage1

---
+++ login/login.js
@@ -0,0 +1,7 @@
+// const loginInputs = document.querySelectorAll(".inputId-Password");
+
+// loginInputs.forEach((input) => {
+//    input.addEventListener("click", (e) => {
+//         e.target.style.border = ""
+//    })
+// })
