23f69751831307c491e5a1150c71910eac9ae931
infiniment <smnoh0707@gmail.com>
2023-08-26 23:19:18 +0900

    loginPage1

--- login/login.js
+++ login/login.js
@@ -1,7 +1,31 @@
-// const loginInputs = document.querySelectorAll(".inputId-Password");
+globalThis.flag = false;
 
-// loginInputs.forEach((input) => {
-//    input.addEventListener("click", (e) => {
-//         e.target.style.border = ""
-//    })
-// })
+const inputId = document.querySelector("input[name=email]");
+const inputPassword = document.querySelector("input[name=password]");
+const loginBtn = document.querySelector(".login-Btn");
+
+inputId.addEventListener("keyup", (e) => {
+    if(!e.target.value || !inputPassword.value){
+        globalThis.flag = false;
+    } else {
+        globalThis.flag = true;
+    }
+    if(globalThis.flag){
+        loginBtn.disabled = false;
+    } else{
+        loginBtn.disabled = true;
+    }
+});
+
+inputPassword.addEventListener("keyup", (e) => {
+    if(!e.target.value || !inputId.value){
+        globalThis.flag = false;
+    } else {
+        globalThis.flag = true;
+    }
+    if(globalThis.flag){
+        loginBtn.disabled = false;
+    } else {
+        loginBtn.disabled = true;
+    }
+});
