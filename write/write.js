a36bd3d8892b1cc7cda44b775a128ba1eb74d7dc
codxexn <codxeun@gmail.com>
2023-08-27 20:42:49 +0900

    backup

--- write/write.js
+++
@@ -1,8 +0,0 @@
-const textarea = document.getElementsByClassName('WritePage_TextArea');
-const button = document.getElementsByClassName('WritePage_Footer');
-
-textarea.addEventLinstener("input", function() {
-  if(!textarea.value){
-    button.style.backgroundColor = rgb(51, 151, 255);
-  }
-});
