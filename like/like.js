9b80e1b337f08e8dbf3571ce77a77731295d6f49
codxexn <codxeun@gmail.com>
2023-08-28 23:33:34 +0900

    설정 페이지 완료

--- like/like.js
+++ like/like.js
@@ -1,25 +1,38 @@
-const pcRedHeart = document.querySelector(".PcRedHeart");
-const pcNoHeart = document.querySelector(".PcNoHeart");
 
-pcRedHeart.addEventListener("click", ()=> {
-  pcRedHeart.style.display = "none";
-  pcNoHeart.style.display = "block";
-})
+// 찜버튼 클릭 시 하트 모양 바뀌는 js
+const heartButtonContainer = document.querySelectorAll(".HeartContainer");
 
-pcNoHeart.addEventListener("click", ()=> {
-  pcNoHeart.style.display = "none";
-  pcRedHeart.style.display = "block";
-})
+heartButtonContainer.forEach((container) => {
+    let firstImg = container.firstElementChild.children[0];
+    let secondImg = container.firstElementChild.children[1];
 
-const redHeart = document.querySelector(".RedHeart");
-const noHeart = document.querySelector(".NoHeart");
+    firstImg.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        e.target.style.display = "none";
+        secondImg.style.display = "block";
+    });
+    secondImg.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.target.style.display = "none";
+        firstImg.style.display = "block";
+    });
+});
 
-redHeart.addEventListener("click", ()=> {
-  redHeart.style.display = "none";
-  noHeart.style.display = "block";
-})
+// 트레이너 찜버튼 클릭 이벤트
+const trainerHeartButtonContainer = document.querySelectorAll(".ProfileHeart");
 
-noHeart.addEventListener("click", ()=> {
-  noHeart.style.display = "none";
-  redHeart.style.display = "block";
-})
+trainerHeartButtonContainer.forEach((container) => {
+  let firstImg = container.children[0];
+  let secondImg = container.children[1];
+
+  firstImg.addEventListener("click", (e) => {
+      e.preventDefault(); 
+      e.target.style.display = "none";
+      secondImg.style.display = "block";
+  });
+  secondImg.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.target.style.display = "none";
+      firstImg.style.display = "block";
+  });
+});
