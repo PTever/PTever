7d6eeab89572e7a4eaa4e7980160fb9022d53ead
infiniment <smnoh0707@gmail.com>
2023-08-28 22:25:00 +0900

    findPasswordPage7

--- find-password/find-password.js
+++ find-password/find-password.js
@@ -1,17 +1,30 @@
-// globalThis.flag = false;
+globalThis.flag = false;
 
-// const inputId = document.querySelector("input[name=email]");
-// const loginBtn = document.querySelector(".send-Email-Btn");
+const inputId = document.querySelector("input[name=email]");
+const sendMailBtn = document.querySelector(".send-Email-Btn");
 
-// inputId.addEventListener("keyup", (e) => {
-//     if(!e.target.value){
-//         globalThis.flag = false;
-//     } else {
-//         globalThis.flag = true;
-//     }
-//     if(globalThis.flag){
-//         loginBtn.disabled = false;
-//     } else{
-//         loginBtn.disabled = true;
-//     }
-// });
+inputId.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        globalThis.flag = false;
+    } else {
+        globalThis.flag = true;
+    }
+    if (globalThis.flag) {
+        sendMailBtn.disabled = false;
+    } else {
+        sendMailBtn.disabled = true;
+    }
+});
+
+function handleResize() {
+    const isMobile = window.innerWidth <= 768;
+    
+    if (isMobile) {
+        sendMailBtn.classList.add("mobile");
+    } else {
+        sendMailBtn.classList.remove("mobile");
+    }
+}
+
+window.addEventListener("resize", handleResize);
+handleResize();
