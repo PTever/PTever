dfa155401a102cb215262a5e34142c54c5ea1eb8
Jimin1030 <pjmjavascript@gmail.com>
2023-08-29 20:24:03 +0900

    쇼핑몰 페이지 첫커밋

--- sign-up-email2/sign-up-email2.js
+++
@@ -1,6 +0,0 @@
-
-const phoneNumInput = document.querySelector("input[name=mobileNumber]");
-const certificationBtn = document.querySelector(".certification-Btn");
-const agreementBtn = document.querySelector(".agreement-Btn");
-
-phoneNumInput.addEventListener("")
