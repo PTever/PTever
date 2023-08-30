3e6bfc35bbeb76008d1d085aaa8349f3c20f17cf
yeseung602 <yeseungg602@gmail.com>
2023-08-30 16:25:34 +0900

    커뮤니티 글쓰기 게시판 수정 2입니다.

--- write/write.js
+++ write/write.js
@@ -5,11 +5,54 @@ const button = document.getElementsByClassName('buttonOff');
 const title = document.getElementById("title");
 const content = document.getElementById("content");
 
-// textarea.addEventLinstener("input", function() {
-//   if(!textarea.value){
-//     button.style.backgroundColor = rgb(51, 151, 255);
-//   }
-// });
+const freePost = document.getElementById("freePost")
+const transaction = document.getElementById("transaction");
+const checkbox = Array.from(document.getElementsByClassName('checkboxItem_Wrapper')); // HTMLCollection을 배열로 변환
+
+
+
+
+// 버튼 키고 끄기
+function buttonOn(button){
+  button.querySelector('.CheckboxItem_Box').classList.add('CheckboxItem_Box_Select');
+  button.querySelector('.CheckboxItem_Box').classList.remove('CheckboxItem_Box');
+  button.querySelector('.CheckboxItem_Input').checked = true;
+}
+
+function buttonOff(button){
+  if (button) {
+    const checkboxBox = button.querySelector('.CheckboxItem_Box_Select');
+    if (checkboxBox) {
+      checkboxBox.classList.add('CheckboxItem_Box');
+      checkboxBox.classList.remove('CheckboxItem_Box_Select');
+    }
+    const checkboxInput = button.querySelector('.CheckboxItem_Input');
+    if (checkboxInput) {
+      checkboxInput.checked = false;
+    }
+  }
+}
+
+// 게시판 글 선택
+checkbox.forEach(button => {
+  button.addEventListener("click", function(){
+    if (button.querySelector('.CheckboxItem_Box')) {
+      buttonOn(button);
+      // 다른 버튼을 선택한 경우에는 이전 버튼을 off로 변경
+      checkbox.forEach(otherButton => {
+        if (otherButton !== button) {
+          buttonOff(otherButton);
+        }
+        return true;
+      });
+    } else {
+      buttonOff(button);
+      return false;
+    }
+
+  })
+});
+
 
 let titleFlag = false;
 let contentFlag = false;
@@ -18,7 +61,6 @@ title.addEventListener("keyup", function(){
   // if(!title.value) titleFlag = true;
   // else titleFlag = false;
 
-  
   console.log(isEmpty(title.value))
   console.log(isEmpty(content.value))
   if(!isEmpty(title.value) && !isEmpty(content.value)) {
@@ -31,7 +73,7 @@ title.addEventListener("keyup", function(){
     console.log(isEmpty(title.value));
     console.log(isEmpty(content.value));
     button[0].style.backgroundColor = 'rgb(221, 221, 221)';
-    button[0].setAttribute('disabled')
+    button[0].setAttribute('disabled', 'true');
   }
 
 })
@@ -66,35 +108,3 @@ function isEmpty(target){
  }
 }
 
-const freePost = document.getElementById("freePost")
-const transaction = document.getElementById("transaction");
-
-console.log(freePost);
-console.log(transaction);
-
-freePost.addEventListener("click", function(e){
-
-  //현재 div의 class 명 가져오기
-  const Box = document.getElementById('Box1');
-  // class명으로 체크여부 판
-  if(Box.className === 'CheckboxItem_Box1'){
-    Box.classList.remove('CheckboxItem_Box1');
-    Box.classList.add('buttonOn');
-  }else if(Box.className === 'buttonOn'){
-    Box.classList.remove('buttonOn');
-    Box.classList.add('CheckboxItem_Box1');
-  }
-})
-
-transaction.addEventListener("click", function(){
- //현재 div의 class 명 가져오기
- const Box = document.getElementById('Box2');
- // class명으로 체크여부 판
- if(Box.className === 'CheckboxItem_Box2'){
-   Box.classList.remove('CheckboxItem_Box2');
-   Box.classList.add('buttonOn');
- }else if(Box.className === 'buttonOn'){
-   Box.classList.remove('buttonOn');
-   Box.classList.add('CheckboxItem_Box2');
- }
-})
