4af7b29c48357407a155e7373a42e439858cf38a
yeseung602 <yeseungg602@gmail.com>
2023-08-30 16:21:23 +0900

    커뮤니티 소거래 장터 수정 2입니다.

--- transaction/transaction.js
+++ transaction/transaction.js
@@ -4,8 +4,10 @@ const prevButtons = document.querySelectorAll('.PaginationButtonGroup_PrevButton
 const nextButtons = document.querySelectorAll('.PaginationButtonGroup_NextButtonWrapper button');
 const firstButton = document.querySelectorAll('.PaginationButtonGroup_PrevButtonWrapper button img');
 const lastButton = document.querySelectorAll('.PaginationButtonGroup_NextButtonWrapper button img');
-
-console.log(firstButton)
+const prev_button = document.getElementById('prev_button');
+const prevprev_button = document.getElementById('prevprev_button');
+const next_button = document.getElementById('next_button');
+const nextnext_button = document.getElementById('nextnext_button');
 
 
 
@@ -15,31 +17,20 @@ buttons.forEach(button => {
       // 이미 선택된 버튼을 다시 클릭한 경우, 아무 작업도 하지 않음
     } else {
       buttons.forEach(e => {
-        e.classList.remove('ButtonPage_StyledButtonOn');
-        e.classList.add('ButtonPage_StyledButton');
+        offchange(e);
       });
 
-      this.classList.remove('ButtonPage_StyledButton');
-      this.classList.add('ButtonPage_StyledButtonOn');
+      onchange(this);
       middlePoint();
+      
 
-      prevButtons.forEach(button => {
-        onChange(button);
-      });
-      nextButtons.forEach(button => {
-        onChange(button);
-      });
+
 
       if (this === buttons[0]) {
         firstPoint();
-        prevButtons.forEach(button => {
-          offChange(button);
-        });
+        
       } else if (this === buttons[buttons.length - 1]) {
         lastPoint();
-        nextButtons.forEach(button => {
-          offChange(button);
-        });
       }
 
       currentButton = this; // 선택된 버튼 업데이트
@@ -47,20 +38,70 @@ buttons.forEach(button => {
   });
 });
 
-function offChange(button) {
-  button.classList.remove('ButtonPage_Styled');
-  button.classList.add('ButtonPage_StyledOff');
-  button.setAttribute('disabled', true);
 
+next_button.addEventListener("click", function() {
+  let currentIndex = Array.from(buttons).indexOf(currentButton);
+
+  if (currentIndex < buttons.length - 1) {
+    middlePoint();
+    offchange(currentButton);
+    currentButton = buttons[currentIndex + 1];
+    onchange(currentButton);
+  } else if (currentIndex === buttons.length - 1) {
+    lastPoint();
+    offchange(currentButton);
+    onchange(buttons[currentIndex]);
+  }
+});
+
+
+
+
+nextnext_button.addEventListener("click", function(){
+  offchange(currentButton);
+  currentButton = buttons[buttons.length-1];
+  onchange(currentButton);
+  lastPoint();
+});
+
+prev_button.addEventListener("click", function() {
+  let currentIndex = Array.from(buttons).indexOf(currentButton);
+  if (currentIndex > 1) { // 버튼 인덱스가 0보다 큰 경우에만 동작
+    middlePoint();
+    offchange(currentButton);
+    currentButton = buttons[currentIndex - 1];
+    onchange(currentButton);
+  } else if (currentIndex === 1) { // 버튼 인덱스가 0인 경우에 firstPoint로 설정
+    firstPoint();
+    offchange(currentButton);
+    currentButton = buttons[currentIndex - 1]; // 인덱스 -1은 유효하지 않음
+    onchange(currentButton);
+  }
+});
+
+
+prevprev_button.addEventListener("click", function() {
+  offchange(currentButton);
+  currentButton = buttons[0];
+  firstPoint();
+  onchange(currentButton);
+});
+
+function offchange(button) {
+  button.classList.remove('ButtonPage_StyledButtonOn');
+  button.classList.add('ButtonPage_StyledButton');
 }
 
-function onChange(button) {
-  button.classList.remove('ButtonPage_StyledOff');
-  button.classList.add('ButtonPage_Styled');
-  button.removeAttribute('disabled');
+function onchange(button) {
+  button.classList.remove('ButtonPage_StyledButton');
+  button.classList.add('ButtonPage_StyledButtonOn');
 }
 
+
+// 화살표 버튼 on/off
 function middlePoint(){
+  prev_button.removeAttribute('disabled')
+  prevprev_button.removeAttribute('disabled')
   for(let i=0; i<firstButton.length; i++){
     if(i==0){
       firstButton[i].src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E %3Cg fill='none' fill-rule='evenodd'%3E %3Crect width='40' height='40' fill='%23FFF' fill-opacity='0' fill-rule='nonzero' rx='20'/%3E %3Cg stroke='%23000' stroke-width='1.5'%3E %3Cpath d='M16 16l4 4-4 4M21 16l4 4-4 4'/%3E %3C/g%3E %3C/g%3E %3C/svg%3E";
@@ -85,6 +126,10 @@ function firstPoint(){
 }
 
 function lastPoint(){
+  prev_button.removeAttribute('disabled')
+  prevprev_button.removeAttribute('disabled')
+  console.log(prev_button)
+  console.log(prevprev_button)
   for(let i=0; i<firstButton.length; i++){
     if(i==0){
       firstButton[i].src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E %3Cg fill='none' fill-rule='evenodd'%3E %3Crect width='40' height='40' fill='%23FFF' fill-opacity='0' fill-rule='nonzero' rx='20'/%3E %3Cg stroke='%23000' stroke-width='1.5'%3E %3Cpath d='M16 16l4 4-4 4M21 16l4 4-4 4'/%3E %3C/g%3E %3C/g%3E %3C/svg%3E";
@@ -95,3 +140,4 @@ function lastPoint(){
     }
   }
 }
+
