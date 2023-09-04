18f585ccd666649190b3646e4588fc32e2c6ec6d
infiniment <smnoh0707@gmail.com>
2023-09-04 21:06:06 +0900

    signUpEmailPage10

--- sign-up-email2/sign-up-email2.js
+++ sign-up-email2/sign-up-email2.js
@@ -1,7 +1,21 @@
+NodeList.prototype.map = Array.prototype.map;
+
 globalThis.flag = new Array([false, false, false, false]);
 
+const inputNickName = document.querySelector("input[name=nickname]");
 const inputPhoneNum = document.querySelector("input[name=mobileNumber]");
 const certificationBtn = document.querySelector(".certification-Btn");
+const inputCertificationNum = document.querySelector("input[name=verificationCode]");
+const certificationBtn2 = document.querySelector(".certification-Btn2"); 
+const allAgreeBtn = document.querySelector(".agreement-Btn");
+const inputPhoneNumMobile = document.querySelector(".form_InputPhoneNum-mobile");
+const certificationBtnMobile = document.querySelector(".certification-Btn-mobile");
+const inputCertificationNumMobile = document.querySelector(".form_InputCheckNum-mobile");
+const certificationBtn2Mobile = document.querySelector(".certification-Btn2-mobile"); 
+const all = document.querySelector("input[name=check-All]");
+const terms = document.querySelectorAll("input[name=check]");
+const allMobile = document.querySelector("input[name=check-All-mobile]");
+const termsMobile = document.querySelectorAll("input[name=check-mobile]");
 
 // 휴대폰 번호 입력시 인증번호 전송 버튼 disabled 사라지게 하는 js
 inputPhoneNum.addEventListener("keyup", (e) => {
@@ -17,11 +31,6 @@ inputPhoneNum.addEventListener("keyup", (e) => {
     }
 });
 
-
-const inputCertificationNum = document.querySelector("input[name=verificationCode]");
-const certificationBtn2 = document.querySelector(".certification-Btn2"); 
-
-
 inputCertificationNum.addEventListener("keyup", (e) => {
     if(!e.target.value){
         globalThis.flag[0] = false;
@@ -36,14 +45,7 @@ inputCertificationNum.addEventListener("keyup", (e) => {
 });
 
 
-// 모두 동의 체크 js 만든 후 닉네임 , 휴대폰 번호 입력 후 인증하기 까지 완료 해서 true로 바꾼다음 모두동의 or 동의까지 true &&이 true가 되면 모두 동의하고 가입하기 버튼 disabled 사라지게
-
-
-
 // 반응형
-
-const inputPhoneNumMobile = document.querySelector(".form_InputPhoneNum-mobile");
-const certificationBtnMobile = document.querySelector(".certification-Btn-mobile");
 
 // 휴대폰 번호 입력시 인증번호 전송 버튼 disabled 사라지게 하는 js
 inputPhoneNumMobile.addEventListener("keyup", (e) => {
@@ -59,8 +61,6 @@ inputPhoneNumMobile.addEventListener("keyup", (e) => {
     }
 });
 
-const inputCertificationNumMobile = document.querySelector(".form_InputCheckNum-mobile");
-const certificationBtn2Mobile = document.querySelector(".certification-Btn2-mobile"); 
 
 
 inputCertificationNumMobile.addEventListener("keyup", (e) => {
@@ -79,17 +79,13 @@ inputCertificationNumMobile.addEventListener("keyup", (e) => {
 
 
 // 모두 동의
-NodeList.prototype.map = Array.prototype.map;
 
 // 전체 동의를 완성하라.
-const all = document.querySelector("input[name=check-All]");
-const terms = document.querySelectorAll("input[name=check]");
 
 all.addEventListener("click", () => {
   terms.forEach((term) => {
     term.checked = all.checked;
   });
-
 });
 
 terms.forEach((term) => {
@@ -102,12 +98,7 @@ terms.forEach((term) => {
 
 // 반응형
 
-// 모두 동의
-NodeList.prototype.map = Array.prototype.map;
-
 // 전체 동의를 완성하라.
-const allMobile = document.querySelector("input[name=check-All-mobile]");
-const termsMobile = document.querySelectorAll("input[name=check-mobile]");
 
 allMobile.addEventListener("click", () => {
     termsMobile.forEach((term) => {
@@ -123,3 +114,21 @@ termsMobile.forEach((term) => {
         .length == 3;
   });
 });
+
+
+
+// 모두 동의 체크 js 만든 후 닉네임 , 휴대폰 번호 입력 후 인증하기 까지 완료 해서 true로 바꾼다음 모두동의 or 동의까지 true &&이 true가 되면 모두 동의하고 가입하기 버튼 disabled 사라지게
+
+// if(!inputNickName.value || !inputPhoneNum.value || !inputCertificationNum.value) {
+//     terms.forEach((term) => {
+//         term.checked != all.checked;
+//     })
+//     globalThis.flag[3] = false;
+// } else {
+//     globalThis.flag[3] = true;
+// }
+// if(globalThis.flag[2] && globalThis.flag[3]) {
+//     allAgreeBtn.disabled = false;
+// } else {
+//     allAgreeBtn.disabled = true;
+// }
